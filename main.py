4f9b423b821ee5aabbf47e1a0a90468dbda56185
Farah Mahmoud <faraheltahawy27@gmail.com>
2025-12-04 13:09:30 +0200

    feat(train): train model, test performance and save it

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from img2vec_pytorch import Img2Vec
 import os
 from PIL import Image
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pickle
+
+# Prepare Data
 
 img2vec = Img2Vec()
 
@@ -23,3 +28,23 @@ for j,dir_ in enumerate([train_dir, val_dir]):
     data[['training_data','validation_data'][j]] = features
     data[['training_labels','validation_labels'][j]] = labels
     
+
+# Train Model
+
+model = RandomForestClassifier()
+model.fit(data['training_data'], data['training_labels'])
+
+
+# Test Performance
+
+y_pred = model.predict(data['validation_data'])
+score = accuracy_score(y_pred, data['validation_labels'])
+
+print("Performance: ", score)
+
+
+# Save Model
+
+with open('./model.p', 'wb') as f:
+    pickle.dump(model, f)
+    f.close()
